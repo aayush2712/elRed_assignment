b84b980d59397e0a24736ab298fd97fe10454236
grimmie <aayush2712@gmail.com>
2022-09-14 18:21:43 +0530

    fin

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ connectDB();
 app.use('/user', user);
 app.use('/task', task);
 
-const PORT = process.env.PORT||5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
+app.listen(PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
